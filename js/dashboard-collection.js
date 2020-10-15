cbce8c84725a297150be49e0c0017e745dd5a186
Spencer Moffat <spencer.g.moffat@gmail.com>
2020-10-15 17:30:00 -0600

    Stylized search API results and checked for null values

--- js/dashboard-collection.js
+++ js/dashboard-collection.js
@@ -2,10 +2,10 @@
 document.getElementById("gameSearchSubmit").addEventListener("click", event => {
   event.preventDefault();
   const searchValue = document.getElementById("gameSearchInput").value;
-  // if (value === "") {
-  //   return;
-  // }
-  console.log("Submitted value = " + searchValue);
+  if (searchValue === "") {
+    return;
+  }
+  // console.log("Submitted value = " + searchValue);
 
 
   //*************** Calling the API ***************
@@ -20,28 +20,63 @@ document.getElementById("gameSearchSubmit").addEventListener("click", event => {
       // API response as JSON
       console.log(json);
 
-      // Invalid submission
-      // if (json.cod === "404") {
-      //   console.log("Invalid API submission: 404 error received");
-      // }
 
+      // Build a long HTML string with all of content from the API call formatted and everything and then insert it into the innerHTML of #gameSearchResults
+      let results = '<div class="row" id="resultsRow" data-aos="zoom-in-up" data-aos-delay="600">';
 
-      // some games are coming back with "null" values for these things, so I should check for that
-      // also, some are coming back with things like 0-0 min where the data is there but clearly wrong
+      // If no game found
+      if (json.games.length === 0) {
+        results += '<div class="col-lg-6 col-xl-4">';
+        results += '<h3 class="no-games-message">No games found. Please check your spelling.</h3></div>';
+      }
 
-      let results = "";
+      // For each game in the array, build a Bootstrap card with its picture, info, and a button to add the game to their collection
       for (let i = 0; i < json.games.length; i++) {
         let game = json.games[i];
         results += '<div class="col-lg-6 col-xl-4"><div class="card mx-auto my-3"><div class="card-body">';
         results += '<h3 class="card-title">' + game.name + '</h3><div class="media">';
         results += '<img class="align-self-center" style="width:100px" src="' + game.images.medium + '">';
-        results += '<div class="media-body">';
-        results += '<ul><li>' + game.min_players + '-' + game.max_players + ' players</li>';
-        results += '<li>' + game.min_playtime + '-' + game.max_playtime + ' min</li>';
-        results += '<li>Released: ' + game.year_published + '</li></ul>';
-        results += '<input class="btn btn-primary btn-block btn-custom-add" type="button" value="Add">'
-        results += '</div></div></div></div></div>'
+        results += '<div class="media-body"><ul>';
+
+        // Player count
+        if (game.min_players === null || game.max_players === null) {
+          results += '<li>Unknown player count</li>';
+        }
+        else if (game.min_players === game.max_players) {
+          if (game.min_players === 1) {
+            results += '<li>' + game.min_players + ' player</li>';
+          }
+          else {
+            results += '<li>' + game.min_players + ' players</li>';
+          }
+        }
+        else {
+          results += '<li>' + game.min_players + '-' + game.max_players + ' players</li>';
+        }
+
+        // Play time
+        if (game.min_playtime === null || game.max_playtime === null) {
+          results += '<li>Unknown play time</li>';
+        }
+        else if (game.min_playtime === game.max_playtime) {
+          results += '<li>' + game.min_playtime + ' min</li>';
+        }
+        else {
+          results += '<li>' + game.min_playtime + '-' + game.max_playtime + ' min</li>';
+        }
+
+        // Release year
+        if (game.year_published === null) {
+          results += '<li>Unknown release year</li>';
+        }
+        else {
+          results += '<li>Released: ' + game.year_published + '</li>';
+        }
+
+        results += '</ul><input class="btn btn-primary btn-block btn-custom-add" type="button" value="Add">';
+        results += '</div></div></div></div></div>';
       }
-      document.getElementById("resultsRow").innerHTML = results;
+      results += '</div>';
+      document.getElementById("gameSearchResults").innerHTML = results;
     });
 });
